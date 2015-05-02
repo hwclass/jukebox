80baf481ef9df9d51207a3c0162f4d87445be643
hwclass <harderworking.class@gmail.com>
2015-05-03 02:44:16 +0300

    Added the song inserting form

--- jukebox-app/jukebox-app.js
+++ jukebox-app/jukebox-app.js
@@ -1,16 +1,29 @@
+/*Collections*/
 Songs = new Mongo.Collection("songs");
 
+/*Meteor Client-Side*/
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
   
   Template.body.helpers({
-    songs: function () {
-      return Songs.find({});  
+    songList: function () {
+      return Songs.find({}, {sort: {createdAt: -1}});  
+    }
+  });
+
+  Template.body.events({
+    'submit .new-song': function (event) {
+      var url = event.target.text.value;
+      Songs.insert({
+        url: url,
+        createdAt: new Date()
+      });
+      event.target.text.value = "";
+      return false;
     }
   });
 }
 
+/*Meteor Server-Side*/
 if (Meteor.isServer) {
   Meteor.startup(function () {
     
