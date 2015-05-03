648817f3323c744695e99b8bf46f3d41c0e6fd87
hwclass <harderworking.class@gmail.com>
2015-05-03 04:47:40 +0300

    Added the android platform

--- jukebox-app/jukebox-app.js
+++ jukebox-app/jukebox-app.js
@@ -1,31 +1,35 @@
-/*Collections*/
 Songs = new Mongo.Collection("songs");
 
-/*Meteor Client-Side*/
 if (Meteor.isClient) {
-  
   Template.body.helpers({
-    songList: function () {
-      return Songs.find({}, {sort: {createdAt: -1}});  
+    songs: function () {
+      return Songs.find({}, {sort: {createdAt: -1}});
     }
   });
 
+  Url = function (artist, title, url, createdAt) {
+    return {
+      artist : artist,
+      title : title,
+      url : url,
+      createdAt : createdAt
+    }
+  }
+
   Template.body.events({
-    'submit .new-song': function (event) {
-      var url = event.target.text.value;
-      Songs.insert({
-        url: url,
-        createdAt: new Date()
-      });
-      event.target.text.value = "";
+    "click #new-song-button": function (event) {      
+      var currentUrl = $(event.target.previousElementSibling)[0].value;
+      var url = new Url('', '', currentUrl.split('v=')[1], new Date());
+      Songs.insert(url);
+      songList.push(url);
+      $(event.target.previousElementSibling)[0].value = "";
       return false;
     }
   });
-}
 
-/*Meteor Server-Side*/
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    
-  });
+  if (typeof Session.set('songList') === 'undefined') {
+    window.songList = [];
+    Session.set('songList', songList);
+  }
+
 }
---
+++ jukebox-app/models/Url.js
@@ -0,0 +1,16 @@
+
+/**
+* Url is the model for song urls
+* @param <String> artist
+* @param <String> title
+* @param <String> url
+* @param <Date> createdAt
+*/
+Url = function (artist, title, url, createdAt) {
+  return {
+    artist : artist,
+    title : title,
+    url : url,
+    createdAt : createdAt
+  }
+};
