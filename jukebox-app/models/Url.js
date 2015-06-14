5761650ec4e4dbb67a7f87acd78c07526756785f
hwclass <harderworking.class@gmail.com>
2015-06-15 01:45:24 +0300

    Integrted the arguments coming from the Song model.

--- jukebox-app/models/Url.js
+++ jukebox-app/models/Url.js
@@ -2,16 +2,14 @@
 
 /**
 * Url is the model for song urls
+* @inherits from Song
 * @param <String> artist
 * @param <String> title
 * @param <String> url
 * @param <Date> createdAt
 */
 Url = function (artist, title, url, createdAt) {
-  return {
-    artist : artist,
-    title : title,
-    url : url,
-    createdAt : createdAt
-  }
+	this.url = url;
+  this.createdAt = createdAt;
+  Song.apply(this, arguments);
 };
